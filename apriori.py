58dff7171231b6a8f1028042b168238870c6dd8f
Finemom <momeni.ui.ac@gmail.com>
2021-05-08 13:37:45 +0430

    lift, sort, analyse

---
+++ apriori.py
@@ -0,0 +1,174 @@
+import sys
+from itertools import chain, combinations
+from collections import defaultdict
+from optparse import OptionParser
+import exploratory
+
+
+transactionList = list()
+unique_items_list = []
+itemSet = set()
+
+
+class Arules:
+
+    largeSet = dict()
+    #transactionList = list()
+    freqSet = defaultdict(int)
+
+    def __init__(self, minSupport, minConfidence):
+        """optparser = OptionParser()
+        optparser.add_option("-f", "--inputFile", dest="input", help="filename containing csv", default=None)
+        optparser.add_option("-s", "--minSupport", dest="minS", help="minimum support value", default=0.15,
+                             type="float", )
+        optparser.add_option("-c", "--minConfidence", dest="minC", help="minimum confidence value", default=0.6,
+                             type="float", )
+        #optparser.add_option("-q", "--questionNum", dest="num", help="number of question", default=None,
+                             #type="int", )
+        (options, args) = optparser.parse_args()
+        inFile = None
+        if options.input is None:
+            self.inFile = sys.stdin
+        elif options.input is not None:
+            self.inFile = self.dataFromFile(options.input)
+        else:
+            print("No dataset filename specified, system with exit\n")
+            sys.exit("System will exit")
+
+        minSupport = options.minS
+        minConfidence = options.minC"""
+        items = self.get_frequent_item_sets(minSupport)
+        rules = self.get_arules(minConfidence)
+        self.printResults(items, rules)
+
+    # returns all subsets of input (with any length)
+    def subsets(self, arr):
+        return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
+
+    # returns candidate subset
+    def returnItemsWithMinSupport(self, itemSet, transactionList, minSupport, freqSet):
+        _itemSet = set()
+        localSet = defaultdict(int)
+
+        # count the frequency of each item in itemSet
+        for item in itemSet:
+            for transaction in transactionList:
+                if item.issubset(transaction):
+                    freqSet[item] += 1
+                    localSet[item] += 1
+
+        # calculate support for each item in itemSet
+        for item, count in localSet.items():
+            support = float(count) / len(transactionList)
+            if support >= minSupport:
+                _itemSet.add(item)
+
+        return _itemSet
+
+    # returns an n-element of k-element itemsets
+    def joinSet(self, itemSet, length):
+        return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
+
+    """def getItemSetTransactionList(self, data_iterator):
+        itemSet = set()
+        for record in data_iterator:
+            transaction = frozenset(record)
+            self.transactionList.append(transaction)
+            for item in transaction:
+                itemSet.add(frozenset([item]))
+                self.unique_items_list.append(item)
+        return itemSet, self.transactionList"""
+
+    def apriori(self, minSupport):
+        #itemSet, self.transactionList = self.getItemSetTransactionList(data_iter)
+        oneCSet = self.returnItemsWithMinSupport(itemSet, transactionList, minSupport, self.freqSet)
+        currentLSet = oneCSet
+        k = 2
+        while currentLSet != set([]):
+            self.largeSet[k - 1] = currentLSet
+            currentLSet = self.joinSet(currentLSet, k)
+            currentCSet = self.returnItemsWithMinSupport(currentLSet, transactionList, minSupport, self.freqSet)
+            currentLSet = currentCSet
+            k = k + 1
+
+    def getSupport(self, item):
+        return float(self.freqSet[item]) / len(transactionList)
+
+    def get_frequent_item_sets(self, minSupport):
+        self.apriori(minSupport)
+        toRetItems = []
+        for key, value in self.largeSet.items():
+            toRetItems.extend([(tuple(item), self.getSupport(item)) for item in value])
+        return toRetItems
+
+    def get_arules(self, minConfidence):
+        toRetRules = []
+        for key, value in list(self.largeSet.items())[1:]:
+            for item in value:
+                _subsets = map(frozenset, [x for x in self.subsets(item)])
+                for element in _subsets:
+                    remain = item.difference(element)
+                    if len(remain) > 0:
+                        confidence = self.getSupport(item) / self.getSupport(element)
+                        if confidence >= minConfidence:
+                            toRetRules.append(((tuple(element), tuple(remain)), confidence))
+        return toRetRules
+
+    def printResults(self, items, rules):
+        for item, support in sorted(items, key=lambda x: x[1]):
+            print("item: %s , %.3f" % (str(item), support))
+        print("\n------------------------ RULES:")
+        for rule, confidence in sorted(rules, key=lambda x: x[1]):
+            pre, post = rule
+            print("Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence))
+
+    """def dataFromFile(self, fname):
+        with open(fname, "rU") as file_iter:
+            for line in file_iter:
+                line = line.strip().rstrip(",")
+                record = frozenset(line.split(","))
+                yield record"""
+
+
+def getItemSetTransactionList(data_iterator):
+    for record in data_iterator:
+        transaction = frozenset(record)
+        transactionList.append(transaction)
+        for item in transaction:
+            itemSet.add(frozenset([item]))
+            unique_items_list.append(item)
+
+
+def dataFromFile(fname):
+    with open(fname, "rU") as file_iter:
+        for line in file_iter:
+            line = line.strip().rstrip(",")
+            record = frozenset(line.split(","))
+            yield record
+
+
+if __name__ == "__main__":
+    optparser = OptionParser()
+    optparser.add_option("-f", "--inputFile", dest="input", help="filename containing csv", default=None)
+    optparser.add_option("-s", "--minSupport", dest="minS", help="minimum support value", default=0.15, type="float", )
+    optparser.add_option("-c", "--minConfidence", dest="minC", help="minimum confidence value", default=0.6, type="float", )
+    optparser.add_option("-q", "--questionNum", dest="num", help="number of question", default=None, type="int", )
+    (options, args) = optparser.parse_args()
+    inFile = None
+    if options.input is None:
+        inFile = sys.stdin
+    elif options.input is not None:
+        inFile = dataFromFile(options.input)
+    else:
+        print("No dataset filename specified, system with exit\n")
+        sys.exit("System will exit")
+
+    getItemSetTransactionList(inFile)
+
+    if options.num == 1:
+        analyse = exploratory.Analysis(transactionList, unique_items_list)
+    else:
+        minSupport = options.minS
+        minConfidence = options.minC
+        apriori = Arules(minSupport, minConfidence)
+
---
+++ exploratory.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mlxtend.preprocessing import TransactionEncoder
+import warnings
+warnings.filterwarnings('ignore')
+
+
+class Analysis:
+
+    def __init__(self, transactionList, unique_items_list):
+        self.analyse(transactionList, unique_items_list)
+
+    def analyse(self, transactionList, unique_items_list):
+        # generating empty dataframe with items as columns
+        df_apriori = pd.DataFrame(columns=unique_items_list)
+        #print(df_apriori)
+
+        te = TransactionEncoder()
+        te_ary = te.fit(transactionList).transform(transactionList)
+        df = pd.DataFrame(te_ary, columns=te.columns_)
+        #print(df.head())
+        #print(df.shape)
+        #print(df.info())
+        df = df.astype('uint8')
+
+        #  ------------------------------- count num of purchased and not purchased for each item
+        zero = []
+        one = []
+        for i in df_apriori.columns:
+            zero.append(list(df[i].value_counts())[0])
+            one.append(list(df[i].value_counts())[1])
+        count_df = pd.DataFrame([zero, one], columns=df_apriori.copy().columns)
+        count_df.index = ['Not_Purchased', 'Purchased']
+        #print(count_df.head())
+    
+        #  ------------------------------- max and min items bought
+        #print('maximum purchased item:', count_df.idxmax(axis=1)[1], ':', count_df.loc['Purchased'].max())
+        #print('minimum purchased item:', count_df.idxmax(axis=1)[0], ':', count_df.loc['Not_Purchased'].max())
+    
+        #  ------------------------------- sorting
+        sorted_df = pd.DataFrame(count_df.sort_values(by=['Purchased'], axis=1, ascending=False).transpose())
+        #print(sorted_df.head(20))
+    
+        #  ------------------------------- adding purchased percentage column
+        #sorted_df['Purchased%'] = sorted_df.Purchased / sum(sorted_df.Purchased)
+        print(sorted_df.head())
+    
+        #  ------------------------------- Finding out average of the total purchased%
+        #print(np.nanmean(sorted_df['Purchased%']))
+    
+        #  ------------------------------- Plotting sorted top purchased products
+        fig = plt.subplots(figsize=(20, 10))
+        purchased = sorted_df.head(50).xs('Purchased', axis=1)
+        purchased.plot(kind='bar', fontsize=16)
+        plt.title('Purchased top Count', fontsize=30)
+        plt.xlabel('Products', fontsize=20)
+        plt.ylabel('total qty. purchased', fontsize=20)
+        #plt.show()
